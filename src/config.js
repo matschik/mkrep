d4a078e4397257a6c3a8ca33c37e8c0950feaf19
Mathieu Schimmerling <mathieu.schimmerling@protonmail.com>
2022-08-21 15:09:01 +0200

    1.0.13

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 import Conf from "conf";
-import fileDirname from "./lib/fileDirname.js";
 import { readFileSync } from "node:fs";
+import { join } from "node:path";
+import fileDirname from "./lib/fileDirname.js";
 
 const { name } = JSON.parse(
   readFileSync(join(fileDirname(import.meta.url), "../../package.json"))
